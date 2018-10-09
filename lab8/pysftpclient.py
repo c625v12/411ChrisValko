854c7057c2eecbe1b2bd8104eb6869b31b8da436
christophervalko <c625v12@github.com>
2018-10-09 11:13:03 -0400

    SMTPTHING

--- lab8/pysftpclient.py
+++ lab8/pysftpclient.py
@@ -1,7 +1,7 @@
 import pysftp, sys
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None
-cinfo = {'cnopts':cnopts, 'host':'oz-ist-linux-fa17-411', 'username':'ftpuser', 'password':'test1234', 'port':101}
+cinfo = {'cnopts':cnopts, 'host':'oz-ist-linux-fa18-411', 'username':'ChristopherValko', 'password':'Lkjhlkjh12', 'port':101}
 try:
     with pysftp.Connection(**cinfo) as sftp:
         print("Connection made")
@@ -10,6 +10,6 @@ try:
             sftp.get('/home/ftpuser/payload.json')
             sftp.remove('/home/ftpuser/payload.json')
         except:
-            print("Log excpetion 1: ", sys.exc_info()[0])
+            print("Log exception  1: ", sys.exc_info()[0])
 except:
     print("Log exception 2: ", sys.exc_info()[0])
